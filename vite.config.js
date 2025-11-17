718063af135223e11d51ec490c210049a0ecdfbd
Mika Leinonen <mika.t.leinonen@google.com>
2025-11-17 12:50:19 +0200

    Corrected base property.

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: '/mtleinon/todolistreact', // add this property and the value is the name of your repository
+	base: '/bookstore/', // add this property and the value is the name of your repository
   plugins: [react()],
 	test: {
     globals: true,
